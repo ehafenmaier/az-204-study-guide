ce365c1c2f45afdbcf6127e4d2e8552430d8caac
Erich Hafenmaier <ehafenmaier@gmail.com>
2023-02-10 11:16:21 -1000

    Add configuration settings for storage connection string

--- az-blob-storage/Program.cs
+++ az-blob-storage/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
+
+
+Console.WriteLine("Azure Blob Storage exercise\n");
+
+IConfiguration config = new ConfigurationBuilder()
+    
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddUserSecrets<Program>()
+    .AddEnvironmentVariables()
+    .Build();
+
+// Run the examples asynchronously, wait for the results before proceeding
+ProcessAsync(config).GetAwaiter().GetResult();
+
+Console.WriteLine("Press enter to exit the sample application.");
+Console.ReadLine();
+
+
+static async Task ProcessAsync(IConfiguration config)
+{
+    // Get storage connection string from configuration settings
+    var storageConectionString = config.GetConnectionString("AzureStorage");
+    
+    // Create blob service client using storage connection string
+    var blobServiceClient = new BlobServiceClient(storageConectionString);
+}
