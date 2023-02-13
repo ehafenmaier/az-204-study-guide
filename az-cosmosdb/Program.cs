1dc99ff91f6e94fc5bee9cceed78082c7a2a95a1
Erich Hafenmaier <ehafenmaier@gmail.com>
2023-02-12 18:58:36 -1000

    Initial commit of cosmosdb console project

---
+++ az-cosmosdb/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddUserSecrets<Program>()
+    .AddEnvironmentVariables()
+    .Build();
+
+// Replace <documentEndpoint> with the information created earlier
+var EndpointUri = config["CosmosDB:Endpoint"];
+
+// Set variable to the Primary Key from earlier.
+var PrimaryKey = config["CosmosDB:AccessKey"];
