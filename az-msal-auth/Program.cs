916969bb520d77c0748b139d6f116e114d936f41
Erich Hafenmaier <ehafenmaier@gmail.com>
2023-02-19 12:20:19 -0700

    Add code to create public client app and acquire access token

--- az-msal-auth/Program.cs
+++ az-msal-auth/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
 
 namespace az_msal_auth;
 
@@ -6,6 +7,7 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
+        // Create configuration settings
         var config = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddUserSecrets<Program>()
@@ -17,5 +19,19 @@ public class Program
 
         Console.WriteLine("Tenant ID: {0}", tenantId);
         Console.WriteLine("Client ID: {0}", clientId);
+        
+        // Create public client application
+        var app = PublicClientApplicationBuilder.Create(clientId)
+            .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
+            .WithRedirectUri("http://localhost")
+            .Build();
+        
+        // Set authentication scopes
+        string[] scopes = { "user.read" };
+        
+        // Acquire access token
+        var result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+        Console.WriteLine("Access Token: {0}", result.AccessToken);
     }
 }
